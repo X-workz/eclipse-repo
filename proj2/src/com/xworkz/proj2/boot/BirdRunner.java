fce328097afebf4c0a0928370eb4030493c4832d
Om <contact@x-workz.in>
2023-01-27 10:15:03 +0530

    added new files

---
+++ proj2/src/com/xworkz/proj2/boot/BirdRunner.java
@@ -0,0 +1,19 @@
+package com.xworkz.proj2.boot;
+
+import com.xworkz.proj2.thing.Bird;
+
+public class BirdRunner {
+
+	public static void main(String[] args) {
+
+		new Bird();//instance
+		new Bird();
+		new Bird();
+		new Bird();
+		new Bird();
+		new Bird();
+		new Bird();
+
+	}
+
+}
---
+++ proj2/src/com/xworkz/proj2/thing/Bird.java
@@ -0,0 +1,9 @@
+package com.xworkz.proj2.thing;
+
+public class Bird {
+
+	public Bird() {
+		System.out.println("Created Bird using no-arg const...");
+	}
+
+}
