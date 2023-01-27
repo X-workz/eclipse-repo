62a8d643f75ff69d626fdc50ead60bd6efbc0cab
Om <contact@x-workz.in>
2023-01-27 10:17:17 +0530

    added new files

--- proj2/src/com/xworkz/proj2/boot/BirdRunner.java
+++ proj2/src/com/xworkz/proj2/boot/BirdRunner.java
@@ -5,8 +5,8 @@ import com.xworkz.proj2.thing.Bird;
 public class BirdRunner {
 
 	public static void main(String[] args) {
-
-		new Bird();//instance
+		System.out.println("running main method in bird runner...");
+		new Bird();// instance
 		new Bird();
 		new Bird();
 		new Bird();
